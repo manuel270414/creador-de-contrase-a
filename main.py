4b65a03d129a586e13ad8ec432d0b8f7a1ff943d
manuel270414 <geraldinecanas2704@gmail.com>
2025-08-26 20:03:07 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+
+caracteres="abcdefghijklmnñopqrstuvwxyz"
+
+a=random.choice(caracteres)
+b=random.choice(caracteres)
+c=random.choice(caracteres)
+d=random.choice(caracteres)
+e=random.choice(caracteres)
+print("hola a continuacion tu contraseña :")
+print(a,b,c,d,e)
